4cd1f8de9dfebae51c2210bc32ca8d95cf2a2f44
agusafriyanto14 <agusafriyanto12345@gmail.com>
2018-12-21 14:06:58 +0700

    Sayur Kool

---
+++ Assets/FREE Food Pack/Script/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+	
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 90 * Time.deltaTime, 90 * Time.deltaTime);
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.name == "Player") 
+		{
+			other.GetComponent<PlayerController> ().points += 10;
+			GetComponent<AudioSource>().Play ();
+			Destroy (gameObject);
+
+		}
+
+	}
+}
---
+++ Assets/FREE Food Pack/Script/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax ;
+
+}
+public class PlayerController : MonoBehaviour 
+{
+	public Rigidbody rb;
+	public float speed;
+	public Boundary boundary;
+
+	public int points = 10;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f);
+
+		rb.velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			);
+		
+	}
+	private void OnGUI()
+	{
+		GUI.Label (new Rect (10, 10, 100, 20), "Score :" + points);
+
+	}
+
+}
